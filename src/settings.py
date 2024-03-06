45052d23bd02e94ffef1484f297aaeb3183e1e4a
divyal-04 <116163832+divyal-04@users.noreply.github.com>
2024-03-06 22:50:10 +0530

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_FROM = 'divyals.patil36@gmail.com'
-EMAIL_HOST_USER = 'divyals.patil36@gmail.com'
-EMAIL_HOST_PASSWORD = 'hslw hnuc susp bdni'
+EMAIL_FROM = 'your_email'
+EMAIL_HOST_USER = 'your_email'
+EMAIL_HOST_PASSWORD = 'your_app_password'
 
